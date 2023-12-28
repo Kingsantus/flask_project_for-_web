e14d478ecb6a8bf169a986a15af37c2a6b0288f0
kingsantus <nkingsantus@gmail.com>
2023-12-28 13:55:09 +0100

    working with database flask_sqlalchemy

--- flask_for_market/app.py
+++ flask_for_market/app.py
@@ -1,11 +1,30 @@
-import sqlite3
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
+#initializing app to accept dictionary
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'market.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 #database initialization
 db = SQLAlchemy(app)
+
+#model initialization
+class Items(db.Model):
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False)
+    description = db.Column(db.String(length=512), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'<Items {self.name}>'
+    
 
 @app.route('/')
 @app.route('/home/')
@@ -14,9 +33,5 @@ def index():
 
 @app.route('/market/')
 def market():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Labtop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
+    items = Items.query.all()
     return render_template('market.html', items=items)
