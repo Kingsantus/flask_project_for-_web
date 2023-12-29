76827d4ceb5ae2d2ba5ea0792adf2ced4b98ce77
kingsantus <nkingsantus@gmail.com>
2023-12-29 01:50:53 +0100

    login required

--- flask_for_market/app/__init__.py
+++ flask_for_market/app/__init__.py
@@ -17,5 +17,7 @@ app.config['SECRET_KEY'] = 'b9a17240484e9064bc6e2c6e'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = 'info'
 
 from app import routes
--- flask_for_market/app/routes.py
+++ flask_for_market/app/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, redirect, url_for, flash
 from app.models import Items, Users
 from app.forms import RegisterForm, LoginForm
 from app import db
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home/')
@@ -11,6 +11,7 @@ def index():
     return render_template('index.html')
 
 @app.route('/market/')
+@login_required
 def market():
     items = Items.query.all()
     return render_template('market.html', items=items)
